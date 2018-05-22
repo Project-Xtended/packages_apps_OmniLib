ef6fc5bea2dd282e72195b0aefe88ac3aab5f106
Juan Ezquerro LLanes <arrase@gmail.com>
2018-05-22 23:27:46 +0200

    Clean libs

--- Android.mk
+++ Android.mk
@@ -8,9 +8,7 @@ LOCAL_MODULE := OmniLib
 LOCAL_SHARED_ANDROID_LIBRARIES := \
     android-support-annotations \
     android-support-v4 \
-    android-support-v7-recyclerview \
     android-support-v7-preference \
-    android-support-v7-appcompat \
     android-support-v14-preference
 
 LOCAL_STATIC_JAVA_LIBRARY := legacy-android-test
